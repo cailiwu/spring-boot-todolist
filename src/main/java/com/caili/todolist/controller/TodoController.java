ef3551b259f2216694d8248f15359088add569c6
CaiLIWu <dy5025115@gmail.com>
2020-09-24 00:51:16 +0800

    Added @ResponseBody

--- src/main/java/com/caili/todolist/controller/TodoController.java
+++ src/main/java/com/caili/todolist/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.caili.todolist.controller;
 import com.caili.todolist.model.entity.Todo;
 import com.caili.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.mongo.ReactiveStreamsMongoClientDependsOnBeanFactoryPostProcessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,22 +25,25 @@ public class TodoController {
         return "todolist";
     }
 
+    @ResponseBody
     @PostMapping("/todos")
-    public String createTodo(@ModelAttribute Todo todo, Model model) {
-        Iterable<Todo> allTodoList = todoService.createTodo(todo);
-        Todo emptyTodo = new Todo();
-        model.addAttribute("todolist", allTodoList);
-        model.addAttribute("todoObject", emptyTodo);
-        return "todolist";
+    public String createTodo(@RequestBody Todo todo) {
+        // @Responsebody后返回结果不会被解析为跳转路径
+       todoService.createTodo(todo);
+        return "OK";
     }
 
+    @ResponseBody
     @PutMapping("/todos/{id}")
-    public void upadteTodo(@PathVariable Integer id, @RequestBody Todo todo) {
+    public String upadteTodo(@PathVariable Integer id, @RequestBody Todo todo) {
         todoService.updateTodo(id ,todo);
+        return "OK";
     }
 
+    @ResponseBody
     @DeleteMapping("/todos/{id}")
-    public void deleteTodo(@PathVariable Integer id) {
+    public String deleteTodo(@PathVariable Integer id) {
         todoService.deleteTodo(id);
+        return "OK";
     }
 }
