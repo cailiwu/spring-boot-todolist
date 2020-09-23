229e1ca5ca3f099ba3f2a395d93a0787a7e3f7d0
CaiLIWu <dy5025115@gmail.com>
2020-09-23 23:20:43 +0800

    Added update and delete controller post and put

--- src/main/java/com/caili/todolist/controller/TodoController.java
+++ src/main/java/com/caili/todolist/controller/TodoController.java
@@ -5,9 +5,9 @@ import com.caili.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.jws.WebParam;
 
 @Controller
 public class TodoController {
@@ -31,5 +31,15 @@ public class TodoController {
         model.addAttribute("todolist", allTodoList);
         model.addAttribute("todoObject", emptyTodo);
         return "todolist";
+    }
+
+    @PutMapping("/todos/{id}")
+    public void upadteTodo(@PathVariable Integer id, @RequestBody Todo todo) {
+        todoService.updateTodo(id ,todo);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public void deleteTodo(@PathVariable Integer id) {
+        todoService.deleteTodo(id);
     }
 }
