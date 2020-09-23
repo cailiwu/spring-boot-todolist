77db49379a7f43d71050f49009ccde4df0e85ff4
CaiLIWu <dy5025115@gmail.com>
2020-09-23 23:20:19 +0800

    Added update and delete service

--- src/main/java/com/caili/todolist/service/TodoService.java
+++ src/main/java/com/caili/todolist/service/TodoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 import java.util.TimeZone;
 
 @Service
@@ -19,12 +20,34 @@ public class TodoService {
     }
 
     public Iterable<Todo> createTodo(Todo todo) {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        df.setTimeZone(TimeZone.getTimeZone("GMT"));
-        String date = df.format(new Date());
-        todo.setCreateTime(date);
-        todo.setUpdateTime(date);
         todoDao.save(todo);
         return getTodo();
+    }
+
+    public Todo updateTodo(Integer id,Todo todo) {
+        try {
+            Todo resTodo = findById(id);
+            Integer status = todo.getStatus();
+            resTodo.setStatus(status);
+            return todoDao.save(resTodo);
+        }catch (Exception exception) {
+            return null;
+        }
+
+    }
+
+    public Todo findById(Integer id) {
+        Todo todo = todoDao.findById(id).get();
+        return todo;
+    }
+
+    public Boolean deleteTodo(Integer id) {
+        try {
+            Todo resTodo = findById(id);
+            todoDao.deleteById(id);
+            return true;
+        }catch (Exception exception) {
+            return false;
+        }
     }
 }
