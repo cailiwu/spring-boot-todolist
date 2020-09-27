f5736178edc4fd7da8ae31b9111bffeb5de783bd
CaiLIWu <dy5025115@gmail.com>
2020-09-27 10:08:42 +0800

    Remove unused code

--- src/main/java/com/caili/todolist/controller/TodoController.java
+++ src/main/java/com/caili/todolist/controller/TodoController.java
@@ -3,14 +3,10 @@ package com.caili.todolist.controller;
 import com.caili.todolist.model.entity.Todo;
 import com.caili.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.mongo.ReactiveStreamsMongoClientDependsOnBeanFactoryPostProcessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.WebParam;
 import java.util.Optional;
 
 @RestController
@@ -39,12 +35,11 @@ public class TodoController {
 
     @PutMapping("/todos/{id}")
     public ResponseEntity upadteTodo(@PathVariable Integer id, @RequestBody Todo todo) {
-        // 呼叫service後，判斷結果，若為true 則回傳ok，反之回傳 400
         Boolean rlt = todoService.updateTodo(id ,todo);
         if (!rlt) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Status 欄位不能為空");
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body("");
+        return ResponseEntity.status(HttpStatus.OK).body("");
     }
 
     @DeleteMapping("/todos/{id}")
--- src/main/java/com/caili/todolist/service/TodoService.java
+++ src/main/java/com/caili/todolist/service/TodoService.java
@@ -5,11 +5,7 @@ import com.caili.todolist.model.entity.Todo;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Optional;
-import java.util.TimeZone;
 
 @Service
 public class TodoService {
@@ -26,16 +22,11 @@ public class TodoService {
     }
 
     public Boolean updateTodo(Integer id,Todo todo) {
-            // get 物件
             Optional<Todo> isExistTodo = findById(id);
-
-            // 檢查是否存在此筆資料
             if (! isExistTodo.isPresent()) {
                 return false;
             }
-            // Assign
             Todo newTodo = isExistTodo.get();
-            // 檢查欄位是否有值
             if (todo.getStatus() == null) {
                 return false;
             }
